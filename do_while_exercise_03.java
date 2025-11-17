e120f3a40383ac4a84212b38c1f3cc089293d807
EdouardVtr <versterren.edouard@keemail.me>
2025-11-17 13:12:39 +0100

    Update do_while_exercise_03.java

--- do_while_exercise_03.java
+++ do_while_exercise_03.java
@@ -19,3 +19,26 @@ public class Main {
         System.out.println("La somme de ces " + count + " nombres est : " + moyenne);
     }
 }
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int nb = 0, count = 0, somme = 0;
+
+        do {
+            somme += nb;
+            ++count;
+            System.out.print("Entrez le " + count + " nombre : ");
+            nb = scan.nextInt();
+            if (nb == -1) {
+                --count;
+            }
+        } while (nb != -1);
+        double moyenne = somme / count;
+        System.out.println("La moyenne des " + count + " est " + moyenne);
+    }
+}
